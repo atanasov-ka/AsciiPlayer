76138c358432cd828966133a51ec6a7b5b1bc2b5
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-07-03 19:47:34 +0300

    Make PlayerFactory create new Player instances out of {Path,File}

--- src/com/asciiplayer/PlayerFactory.java
+++ src/com/asciiplayer/PlayerFactory.java
@@ -1,10 +1,14 @@
 package com.asciiplayer;
 
 import java.io.File;
+import java.nio.file.Path;
 
 public class PlayerFactory {
-	public static Player get(File file)
-	{
+	public static Player newPlayer(Path file) {
 		return null;
+	}
+	
+	public static Player newPlayer(File file) {
+		return newPlayer(file.toPath());
 	}
 }
