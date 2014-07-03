ec75c334ca066c5c1d677b428fe28a1e29c7ad55
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-07-03 21:57:16 +0300

    Implement GifPlayer

--- src/com/asciiplayer/GifPlayer.java
+++ src/com/asciiplayer/GifPlayer.java
@@ -1,28 +1,30 @@
 package com.asciiplayer;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.imageio.ImageIO;
-
-
-
 public class GifPlayer extends SequencePlayer {
-	GifPlayer(Path path)
-	{
-		super(path);
-	}
-
-	@Override
-	public void play() {
-		BufferedImage gif = ImageIO.read(getSource());
-		
-	}
-
-	@Override
-	protected List<String> convert(Path path) {
-		// TODO Auto-generated method stub
-		return null;
+	GifPlayer(Path source) {
+		super(source);
 	}
 	
+	GifPlayer(File source) {
+		super(source.toPath());
+	}
+
+	@Override
+	public List<String> convert(Path path) {
+		GifDecoder decoder = new GifDecoder();
+		List<String> frames = new ArrayList<String>();
+		
+		BufferedImage frame;
+		for (int i = 0, len = decoder.getFrameCount(); i < len; i++) {
+			frame = decoder.getFrame(i);
+			frames.add(PlayerUtils.stringify(frame, 100));
+		}
+		
+		return frames;
+	}
 }
--- src/com/asciiplayer/SequencePlayer.java
+++ src/com/asciiplayer/SequencePlayer.java
@@ -1,56 +1,45 @@
 package com.asciiplayer;
 
-
 import java.io.File;
 import java.nio.file.Path;
-import java.util.LinkedList;
 import java.util.List;
 
 public abstract class SequencePlayer extends FilePlayer {
-	public SequencePlayer(Path path) {
-		super(path);
+	public SequencePlayer(Path source) {
+		super(source);
 	}
-	
-	protected abstract List<String> convert (Path path);
-	
-	public void play() throws Exception {}
-	{
-		//int lastFrameLinesCount = 0;
+
+	public SequencePlayer(File source) {
+		this(source.toPath());
+	}
+
+	protected abstract List<String> convert(Path path);
+
+	public void play() throws Exception {
 		List<String> list = convert(getSource());
 		for (String string : list) {
 			System.out.println(string);
-			
-			/*
-			for (int i = 0; i < string.length(); ++i)
-			{
-				if (string.charAt(i) == '\n')
-					++lastFrameLinesCount;
-			}
-			*/
-			
+
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-				
+
 			clearConsole();
 		}
 	}
-	
-	public final static void clearConsole()
-	{
-	    try {
-	        final String os = System.getProperty("os.name");
-	        if (os.contains("Windows")) {
-	            Runtime.getRuntime().exec("cls");
-	        }
-	        else {
-	            Runtime.getRuntime().exec("clear");
-	        }
-	    }
-	    catch (final Exception e) {
-	    	e.printStackTrace();
-	    }
+
+	public final static void clearConsole() {
+		try {
+			final String os = System.getProperty("os.name");
+			if (os.contains("Windows")) {
+				Runtime.getRuntime().exec("cls");
+			} else {
+				Runtime.getRuntime().exec("clear");
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
