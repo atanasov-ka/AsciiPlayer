1b9cd1f842067ffa4a8f3c8cbe304cdad5479d23
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-07-03 21:03:45 +0300

    Refactor the base FilePlayer implementation

--- src/com/asciiplayer/PicturePlayer.java
+++ src/com/asciiplayer/PicturePlayer.java
@@ -1,23 +1,24 @@
 package com.asciiplayer;
 
 import java.awt.image.BufferedImage;
-import java.io.IOError;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
 import javax.imageio.ImageIO;
 
-
 public class PicturePlayer extends FilePlayer {
+	public PicturePlayer(Path source) {
+		super(source);
+	}
 	
-	public PicturePlayer(Path path)
-	{
-		super(path);
+	public PicturePlayer(File source) {
+		super(source.toPath());
 	}
 
 	@Override
 	public void play() throws IOException {
-		BufferedImage image = ImageIO.read(getPath().toFile());
+		BufferedImage image = ImageIO.read(getSource().toFile());
 		System.out.println(PlayerUtils.stringify(image, 100));
 	}
 }
